c0c73d6fe02a6ea37702dcbc92b0729aed5f5219
Samuel Sacco <SamLSacco@aol.com>
2021-07-02 16:20:38 -0400

    heroku push

---
+++ frontend/samcloud.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Root from './components/root'
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     // const root = document.getElementById("root");
+//     const root = "hello"
+//     ReactDOM.render(<h1>Welcome to SamCloud</h1>, root);
+// });
